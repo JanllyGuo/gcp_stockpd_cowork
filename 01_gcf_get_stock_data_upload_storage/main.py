933c567107d0459af75cf3a9965b7979c6714923
Janlly <114202264+JanllyGuo@users.noreply.github.com>
2022-11-09 01:58:02 +0800

    Update main.py

--- 01_gcf_get_stock_data_upload_storage/main.py
+++ 01_gcf_get_stock_data_upload_storage/main.py
@@ -6,8 +6,8 @@ from google.cloud import storage
 
 def hello_world(request):
 	stock_list = ['2303', '2317', '2327', '2330', '2345', '2377', '2395', '2409', '2454', '3037', '3481', '3532', '6415', '8046']
-    startDate = '2014-01-01'
-    #today = date.today()
+    	startDate = '2014-01-01'
+    	#today = date.today()
 	
 	def upload_storage(sourcefile, dstfile, bucketname):
 		try:
@@ -21,11 +21,11 @@ def hello_world(request):
 
 	for stockid in stockid_list:
 		#stockid = stockid.strip()
-        data = yf.download(f'{stockid}.Tw', startDate)
-        data.drop('Adj Close', axis=1, inplace=True)
-        csvfile = data.to_csv()
-        print(f'Load {stockid} data done')
+        	data = yf.download(f'{stockid}.Tw', startDate)
+        	data.drop('Adj Close', axis=1, inplace=True)
+        	csvfile = data.to_csv()
+        	print(f'Load {stockid} data done')
 		
-		# $bucket_name : 須修改為你自己創建的Cloud Storage Bucket
-		upload_storage(csvfile, f'stock-data/{stockid}.csv', '$bucket_name)
+		# Your bucket name -> replace Your Cloud Storage Bucket name
+		upload_storage(csvfile, f'stock-data/{stockid}.csv', 'Your bucket name')
 		print(f'Upload {stockid} data to storage done')
